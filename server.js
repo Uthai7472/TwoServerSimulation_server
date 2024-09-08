f58ec82cdd46617155469ffbc1eb3d6c068cc36a
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-08 19:18:36 +0700

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 // server1/index.js
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
 const PORT = 5001;
 
-app.use(bodyParser.json());
+const bodyParser = require('body-parser');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Allow requests from your Netlify frontend URL
+const allowedOrigins = ['https://io-learning-client.netlify.app/', 'https://two-server-simulation-client.vercel.app'];  // Add your Netlify URL here
+
 app.use(cors({
-    origin: '*'
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like curl requests or Postman)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
+
+app.use(bodyParser.json());
 
 // Store commands in memory for simplicity
 let commandToSend;
@@ -20,6 +36,9 @@ app.post('/command', (req, res) => {
     // commands.push(command);
     commandToSend = command
     console.log(command, commandToSend);
+
+    // Socket io implement
+    io.emit('command', command); // Emit command to all connected clients
     res.status(200).send('Command received');
 });
 // Endpoint to serve commands
@@ -34,6 +53,8 @@ let receivedRandomValue = null;
 app.post('/random', (req, res) => {
     const { randomValue } = req.body;
     receivedRandomValue = randomValue;
+    // Socket io implement
+    io.emit('randomValue', receivedRandomValue); // Emit random value to all connected clients
     console.log('Received random value from server2:', receivedRandomValue);
     res.status(200).send('Random value received');
 });
@@ -43,6 +64,6 @@ app.get('/random', (req, res) => {
     res.status(200).json(receivedRandomValue);
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server1 running on http://localhost:${PORT}`);
 });
