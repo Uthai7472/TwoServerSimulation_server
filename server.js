8c1052c2cc332e8f0d8af567e809865448505916
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-09 11:11:38 +0700

    Updated README and fixed bugs

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: '*' } });
 
+let latestRandomValue = null;
+
 io.on('connection', (socket) => {
     console.log('Server1: A client connected');
 
     socket.on('randomValue', (value) => {
         console.log(`Server1 received random value: ${value}`);
+        latestRandomValue = value;
         io.emit('randomValue', value); // Forward to React1
     });
 
@@ -19,6 +22,10 @@ io.on('connection', (socket) => {
         console.log('Server1: A client disconnected');
     });
 });
+
+app.get('/', (req, res) => {
+    res.send({ randomValue: latestRandomValue });
+})
 
 server.listen(3001, () => {
     console.log('Server1 is running on port 3001');
