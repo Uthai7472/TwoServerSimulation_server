8d875d9635ebb6304e31688836c28959d5a10641
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-09 11:16:58 +0700

    Updated README and fixed bugs

--- server.js
+++ server.js
@@ -2,10 +2,16 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({
+    origin: '*'
+}));
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: '*' } });
+
+
 
 let latestRandomValue = null;
 
