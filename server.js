71461da6fbfe6fc6f0178ab7928102028071d6c5
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-13 15:30:53 +0700

    First commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = 3000;
 
 let sensorData;
+let command1, command2;
 
 // Middleware to parse JSON data
 app.use(express.json());
@@ -19,6 +20,21 @@ app.post('/api/data', (req, res) => {
 app.get('/api/data', (req, res) => {
 
   res.json(sensorData);
+});
+
+// Get command from client
+app.post('/api/command', (req, res) => {
+  command1 = req.body.command[0];
+  command2 = req.body.command[1];
+
+  console.log('Received command from client:', command1);
+  console.log('Received command from client:', command2);
+
+  const response = {message: 'Command from client received'};
+  res.json(response);
+});
+app.get('/api/command', (req, res) => {
+  res.json({command1: command1, command2: command2});
 });
 
 const server = app.listen(port, () => {
