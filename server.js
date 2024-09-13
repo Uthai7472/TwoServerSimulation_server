cb8e085ebf8aec154c1b9524ae3131ef01997584
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-13 08:13:03 +0700

    First commit

--- server.js
+++ server.js
@@ -1,57 +1,32 @@
-const mqtt = require('mqtt');
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
-
-// MQTT broker details
-const MQTT_BROKER = "mqtt://test.mosquitto.org"; // Replace with your broker URL
-const MQTT_TOPIC = "my/random/topic";
-
-// Create an Express app
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
   cors: {
-    // origin: 'http://localhost:5173', // Replace with your React app's URL
+    origin: ['https://two-server-simulation-client.vercel.app/', 'http://localhost:5173'],
     methods: ['GET', 'POST'],
-    credentials: true
+    allowedHeaders: ['content-type']
   }
 });
+const port = process.env.PORT || 3000;
+const cors = require('cors');
 
-// Variable to store the latest message
-let latestMessage = "";
+app.use(cors());
 
-// Connect to the MQTT broker
-const client = mqtt.connect(MQTT_BROKER);
-
-client.on('connect', () => {
-  console.log('Connected to MQTT broker');
-  client.subscribe(MQTT_TOPIC);
-});
-
-client.on('message', (topic, message) => {
-  console.log(`Received message: ${message.toString()}`);
-  latestMessage = message.toString(); // Store the latest message
-  io.emit('mqtt-data', latestMessage); // Emit to Socket.IO clients
-});
-
-// Set up Socket.IO for real-time communication
 io.on('connection', (socket) => {
-  console.log('Client connected');
+  console.log('A Raspberry Pi connected');
+
+  socket.on('raspberry-pi-data', (data) => {
+    console.log('Received sensor data:', data);
+    // Process the sensor data and store it, if needed
+    io.emit('sensor-data', data);
+  });
+
+  setInterval(() => {
+    socket.emit('server-command', { action: 'update_led', state: 'on' });
+  }, 5000);
 });
 
-// Enable CORS
-app.use(cors({
-  // origin: 'http://localhost:5173'
-}));
-
-// HTTP endpoint to send the latest message
-app.get('/latest-message', (req, res) => {
-  res.send({ message: latestMessage }); // Send the latest message as a JSON response
-});
-
-// Start the server
-server.listen(3000, () => {
-  console.log('Node.js server listening on port 3000');
+http.listen(port, () => {
+  console.log('Server listening on port 3000');
 });
